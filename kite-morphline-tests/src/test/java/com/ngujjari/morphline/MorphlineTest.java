2f7ac0aa92f8dfa100576d47e8c8a56a5afc8005
gujjari.naresh@gmail.com <ngujjari@Nareshs-MacBook-Air.local>
2016-08-14 21:18:02 -0400

    Added generic webserver logs command

--- kite-morphline-tests/src/test/java/com/ngujjari/morphline/MorphlineTest.java
+++ kite-morphline-tests/src/test/java/com/ngujjari/morphline/MorphlineTest.java
@@ -12,35 +12,58 @@ import org.kitesdk.morphline.base.Fields;
 
 public class MorphlineTest extends ExampleMorphlineTest {
 
-  
-  
-  @Test
-  public void testWebServerLogs() throws Exception {
-    morphline = createMorphline("test-morphlines/webserverLogs");
-    InputStream in = new FileInputStream(new File(RESOURCES_DIR + "/test-documents/webserverLogs.log"));
-    Record record = new Record();
-    record.put(Fields.ATTACHMENT_BODY, in);
-    record.put(Fields.ATTACHMENT_MIME_TYPE, "text/plain");
-    
-    assertTrue(morphline.process(record));
-    Record expected = new Record(); 
-    expected.put("ip", "125.158.192.193");
-    assertTrue(collector.getRecords().size() > 0);    
-  }
-  
-  @Test
-  public void testLog4jLogs() throws Exception {
-    morphline = createMorphline("test-morphlines/log4jLogs");
-    InputStream in = new FileInputStream(new File(RESOURCES_DIR + "/test-documents/log4jLogs.log"));
-    Record record = new Record();
-    record.put(Fields.ATTACHMENT_BODY, in);
-    record.put(Fields.ATTACHMENT_MIME_TYPE, "text/plain");
-    
-    assertTrue(morphline.process(record));
-    Record expected = new Record(); 
-    expected.put("ip", "125.158.192.193");
-    assertTrue(collector.getRecords().size() > 0);    
-  }
-  
+	
+	@Test
+	public void testWebserverpr1was6access() throws Exception 
+	{
+		morphline = createMorphline("Logs/webserverpr1access");
+		InputStream in = new FileInputStream(new File(RESOURCES_DIR + "/Logs/webserverpr1was6access.log"));
+		Record record = new Record();
+		record.put(Fields.ATTACHMENT_BODY, in);
+		record.put(Fields.ATTACHMENT_MIME_TYPE, "text/plain"); 
+
+		assertTrue(morphline.process(record));
+		assertTrue(collector.getRecords().size() > 0);    
+	}
+	
+
+	@Test
+	public void testWebserverpr1access() throws Exception 
+	{
+		morphline = createMorphline("Logs/webserverpr1access");
+		InputStream in = new FileInputStream(new File(RESOURCES_DIR + "/Logs/webserverpr1access.log"));
+		Record record = new Record();
+		record.put(Fields.ATTACHMENT_BODY, in);
+		record.put(Fields.ATTACHMENT_MIME_TYPE, "text/plain"); 
+
+		assertTrue(morphline.process(record));
+		assertTrue(collector.getRecords().size() > 0);    
+	}
+
+
+	@Test
+	public void testWebServerLogs() throws Exception {
+		morphline = createMorphline("test-morphlines/webserverLogs");
+		InputStream in = new FileInputStream(new File(RESOURCES_DIR + "/test-documents/webserverLogs.log"));
+		Record record = new Record();
+		record.put(Fields.ATTACHMENT_BODY, in);
+		record.put(Fields.ATTACHMENT_MIME_TYPE, "text/plain");
+
+		assertTrue(morphline.process(record));
+		assertTrue(collector.getRecords().size() > 0);    
+	}
+
+	@Test
+	public void testLog4jLogs() throws Exception {
+		morphline = createMorphline("Logs/log4jLogs");
+		InputStream in = new FileInputStream(new File(RESOURCES_DIR + "/Logs/log4jLogs.log"));
+		Record record = new Record();
+		record.put(Fields.ATTACHMENT_BODY, in);
+		record.put(Fields.ATTACHMENT_MIME_TYPE, "text/plain");
+		record.put("server", "EntpServicesA1");
+		assertTrue(morphline.process(record));
+		assertTrue(collector.getRecords().size() > 0);    
+	}
+
 
 }
